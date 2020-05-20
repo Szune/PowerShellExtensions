bb28922463102e27aab609c8b4381b22bdbd07e5
Szune <erikiwarson@gmail.com>
2020-05-20 15:48:22 +0200

    adds -All switch to part, shows all matches

--- profile.ps1
+++ profile.ps1
@@ -27,19 +27,33 @@ function grep {
     }
 }
 
+
 function part {
     [CmdletBinding()]
     Param(
     [Parameter(ValueFromPipeline)] $Input,
     [Parameter(Position = 1)] $Pattern,
     [Parameter(Position = 2)] $Part,
-    [Parameter(Position = 3)] $Idx)
+    [Parameter(Position = 3)] $Idx,
+    [switch] $All)
     process {
         $str = $Input | Out-String -Stream
         $matches = ([regex]$Pattern).Matches($str)
+        if($All) {
+            $mc = 0
+            Write-Host "Found $($matches.Count) match(es)"
+            $matches.ForEach({
+                Write-Host "> Index $mc`: $_"
+                for($i = 0; $i -lt $_.Groups.Count; $i++) {
+                    Write-Host "-- Group $($_.Groups[$i].Name): $($_.Groups[$i].Value)"
+                }
+                $mc++
+            })
+            return
+        }
         if($matches.Count -gt 0) {
             if($Part) {
-                if($Idx -ne $null) {
+                if($Idx -is [int]) {
                     return $matches[$Idx].Groups[$Part].Value
                 } else {
                     return $matches[0].Groups[$Part].Value
